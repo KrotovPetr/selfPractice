79c5c07f53d93fda251399bbb06577e07c40131d
Krotov Petr <83280920+KrotovPetr@users.noreply.github.com>
2022-10-08 22:00:12 +0300

    add some answers to YaYoung summer22

---
+++ YaYoung/summer20/ex1.js
@@ -0,0 +1,17 @@
+function ex1(N, staff, K) {
+    if (K===0){
+        return 0
+    }
+    staff = staff.sort((a,b)=>{
+        if (a < b) return 1;
+        if (a === b) return 0;
+        if (a > b) return -1; });
+    let sum = 0;
+    for(let i=0;i<K;i++){
+        sum+=staff[i];
+    }
+    return sum;
+
+}
+
+console.log( ex1(12,  [22, 7, 24, 24, 11, 22, 24, 3, 9, 16, 2, 19],  7))
