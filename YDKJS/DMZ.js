3c1e72d4460e609d748ef5dafeca119844d8e45c
Krotov Petr <83280920+KrotovPetr@users.noreply.github.com>
2022-10-17 19:46:59 +0300

    add some solutions to YDKJS

---
+++ YDKJS/DMZ.js
@@ -0,0 +1,20 @@
+// function foo() {
+//     console.log( "a:" + a + ", b:" + b );
+// }
+//
+// // наш пустой DMZ-объект
+// var ø = Object.create( null );
+//
+// // распаковываем массив как параметры
+// foo.apply( ø, [2, 3] ); // a:2, b:3
+//
+// // каррируем с помощью `bind(..)`
+// var bar = foo.bind( ø, 2 );
+// bar( 3 ); // a:2, b:3
+function checKJSON(){
+    let a = {c:3}
+    let obj = {a: {c:a, foo: ()=>{console.log(1)}}}
+    console.log("c" in obj)
+}
+
+checKJSON()
